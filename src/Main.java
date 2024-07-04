e1fc6f9ab9e67984fe8d8f3facc5ef86260197db
Joaquin Nietes <108995566+JNietes@users.noreply.github.com>
2024-07-04 15:18:27 -0400

    Getting element tags

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        WebPage wp1 = new WebPage("https://jnietes.github.io/mine-sweeper-webpage");
+        WebPage wp1 = new WebPage("https://jnietes.github.io/mine-sweeper-webpage/");
         ArrayList<String> wp1AL = wp1.createHTMLArrayList();
+        ArrayList<String> wp1TAL = wp1.findTags(wp1AL);
         System.out.println(wp1AL);
+        System.out.println(wp1TAL);
     }
 }
