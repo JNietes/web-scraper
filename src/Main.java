e7463d8ba1b792383fc52968e696b86ef7e8d7ec
Joaquin Nietes <108995566+JNietes@users.noreply.github.com>
2024-07-22 11:37:12 -0400

    Refactor

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ public class Main {
     public static void main(String[] args) {
         WebPage wp1 = new WebPage("https://jnietes.github.io/mine-sweeper-webpage/");
         System.out.println(wp1.getHTMLAL().size());
-        //System.out.println(wp1.getHTMLAL());
-        //System.out.println(wp1TAL);
         wp1.createHTMLTree();
-        //WebPage wp2 = new WebPage("https://www.google.com/search?q=Entry+Level+Software+jobs&oq=google+job+searcher&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhA0gEIOTQxNGowajeoAgiwAgE&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&ved=2ahUKEwj-q6emi5CHAxUTF1kFHdU1AOMQutcGKAF6BAgiEAQ&sxsrf=ADLYWIK1FoGnD8mo-9Iu6uJnKiBl7mP1HA:1720188974789#fpstate=tldetail&htivrt=jobs&htidocid=gZFgPi8VsYPHYHN7AAAAAA%3D%3D");
-        //System.out.println(wp2.getHTMLAL().size());
-        //System.out.println(wp2.findTags());
     }
 }
--- src/WebPage.java
+++ src/WebPage.java
@@ -25,18 +25,15 @@ public class WebPage {
                 while (!HTMLAL.get(i).equals(">")) {
                     element.append(HTMLAL.get(++i));
                 }
-                if (isHTMLElement(element.toString())) {
-                    nodes.add(element.toString());
-                    endOfLastTag = i;
-                }
                 if (element.toString().startsWith("</") && !HTMLAL.get(endOfLastTag + 1).equals(" ") &&  !HTMLAL.get(endOfLastTag + 1).equals("<")) {
                     for (int j = endOfLastTag+1; j <= i - element.length(); j++) {
                         text.append(HTMLAL.get(j));
                     }
                     nodes.add(text.toString());
                     text.setLength(0);
-                    endOfLastTag = i;
                 }
+                endOfLastTag = i;
+                nodes.add(element.toString());
                 element.setLength(0);
             }
         }
@@ -61,35 +58,6 @@ public class WebPage {
         catch (IOException ex) {
             System.out.println("I/O Errors: no such file");
         }
-    }
-
-    public ArrayList<String> findTags() {
-        ArrayList<String> tagArrayList = new ArrayList<>();
-        StringBuilder elementTag = new StringBuilder();
-        for (int i=0; i<HTMLAL.size(); i++) {
-            if (HTMLAL.get(i).equals("<")) {
-                elementTag.append("<");
-                while (!HTMLAL.get(i).equals(">")) {
-                    elementTag.append(HTMLAL.get(++i));
-                }
-                tagArrayList.add(elementTag.toString());
-                elementTag.setLength(0);
-            }
-        }
-        return tagArrayList;
-    }
-
-    private boolean isHTMLElement(String tag) {
-        String[] notCrumbElements = {"!", "/", "input", "link"};
-        boolean temp = true;
-        for (String element : notCrumbElements) {
-            if (tag.startsWith(element, 1)) {
-                temp = false;
-                break;
-            }
-        }
-
-        return temp;
     }
 
     public ArrayList<String> getHTMLAL() {
